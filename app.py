c81c3e4d10ad3c9e19f74fec28827cb3afc867f5
samirsayyed5836 <95004194+samirsayyed5836@users.noreply.github.com>
2023-09-24 12:13:17 +0000

    added dynamic pages

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 from flask import Flask, jsonify, render_template
 
-from database import load_jobs_form_db
+from database import load_job_from_db, load_jobs_form_db
 
 app = Flask(__name__)
-
-
 
 
 @app.route("/")
 def hello_world():
   jobs = load_jobs_form_db()
-  return render_template("home.html", jobs=jobs, company_name="Samir")
+  return render_template("home.html", jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_form_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    
+    if job is None:
+        return jsonify({"error": "Job not found"}), 404  # Return a 404 response for not found
+    
+    # Convert the "RowMapping" object to a dictionary
+    job_dict = dict(job)
+    
+    return render_template("jobpage.html" , job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -9,9 +9,6 @@ engine = create_engine(db_connection_string,
                        }})
 
 
-
-
-
 def load_jobs_form_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
@@ -20,6 +17,15 @@ def load_jobs_form_db():
      jobs.append(row._mapping)
     return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs WHERE id = :val").bindparams(val=id))
+    
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._mapping
 
 
 
